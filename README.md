f2d973fbdf8b44ff22f258905742d203758701f8
BlinovA25 <antoha2550@mail.ru>
2022-10-30 15:09:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Chat2
+
+To start your Phoenix server:
+
+  * to install dependencies `mix deps.get`
+  *  set source `source ~/.asdf/asdf.sh`
+  * Setup the project with `mix setup`
+  * Start Phoenix endpoint with `mix phx.server`
+
+Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+Ready to run in production? Please [check our deployment guides](https://hexdocs.pm/phoenix/deployment.html).
+
+## Learn more
+
+  * Official website: https://www.phoenixframework.org/
+  * Guides: https://hexdocs.pm/phoenix/overview.html
+  * Docs: https://hexdocs.pm/phoenix
+  * Forum: https://elixirforum.com/c/phoenix-forum
+  * Source: https://github.com/phoenixframework/phoenix
---
+++ lib/chat_2_web/live/room_live.ex
@@ -0,0 +1,41 @@
+defmodule Chat2Web.RoomLive do
+  use Chat2Web, :live_view
+  require Logger
+
+  # Connecting to the current room
+  @impl true
+  def mount(%{"id" => room_id}, _session, socket) do
+    topic = "room:" <> room_id
+    username = MnemonicSlugs.generate_slug(2)
+    if connected?(socket), do: Chat2Web.Endpoint.subscribe(topic)
+
+    {:ok, assign(socket,
+      room_id: room_id,
+      topic: topic,
+      username: username,
+      message: "",
+      messages: [%{uuid: UUID.uuid4(), content: "#{username} joined the chat"}],
+      temporary_assigns: [messages: []])
+    }
+  end
+
+  # Handling of form submit action
+  @impl true
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    message = %{uuid: UUID.uuid4(), content: message}
+    Chat2Web.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, assign(socket, message: "")}
+  end
+
+  # Clearing up the form
+  @impl true
+  def handle_event("form_changed", %{"chat" => %{"message" => message}}, socket) do
+    {:noreply, assign(socket, message: message)}
+  end
+
+  # Handler for adding new message to list assingned to current socket
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    {:noreply, assign(socket, messages: [message])}
+  end
+end
